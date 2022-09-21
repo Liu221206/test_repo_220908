1fc739ee25459c92773968538bbc20cae3b22120
Liu221206 <112857432+Liu221206@users.noreply.github.com>
2022-09-21 16:02:06 +0900

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,16 @@
+
+
+
+
+
+const getRawSkillTarget = (
+  id: string
+): SkillTarget | undefined => {
+  return rawSkillTarget.find(it => it.id === id)
+}
+
+function getRawSkillTarget(
+  id: string
+): SkillTarget | undefined {
+  return rawSkillTarget.find(it => it.id === id)
+}
